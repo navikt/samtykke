f0d55518a97c184982048d16287332120a55d43c
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-12-12 12:01:19 +0100

    Add basic tests for landing page

--- employee/cypress/e2e/landing.cy.ts
+++ employee/cypress/e2e/landing.cy.ts
@@ -1,5 +1,29 @@
-describe('empty spec', () => {
-    it('passes', () => {
+describe('Landing page is loaded propperly', () => {
+    it('Employee name loaded', () => {
         cy.visit('http://localhost:5173/ansatt/')
+
+        cy.contains('Dan Børge')
+    })
+
+    it('Loads active consents', () => {
+        cy.visit('http://localhost:5173/ansatt/')
+
+        cy.get('*[class^="navds-accordion"]')
+            .find('*[class^="navds-accordion__item"]')
+            .should('have.length', 3)
+    })
+
+    it('Active consent contain consent-code', () => {
+        cy.visit('http://localhost:5173/ansatt/')
+
+        cy.get('*[class^="navds-accordion"]')
+            .children()
+            .eq(0)
+            .click()
+            .children()
+            .get('b')
+            .each(($el, index, $list) => {
+                cy.wrap($el).invoke('text').should('have.length', 7)
+            })
     })
 })
