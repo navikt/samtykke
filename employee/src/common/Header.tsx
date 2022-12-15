710695982f6259535ba041f73f4db765815936ff
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-12-15 13:53:26 +0100

    Add routing to landing page

--- employee/src/common/Header.tsx
+++ employee/src/common/Header.tsx
@@ -1,14 +1,22 @@
 import React, { ReactElement } from 'react'
-import { Button, Heading, Link } from '@navikt/ds-react'
+import { Button, Heading } from '@navikt/ds-react'
 import { Logout, Email, People } from '@navikt/ds-icons'
+import { useNavigate } from 'react-router-dom'
 
 export default function Header(): ReactElement {
+
+    const navigate = useNavigate()
+
     return (
         <div className='w-full m-0 flex flex-row justify-between bg-[#fefefe] p-12'>
             <Heading size='xlarge'>Samtykke</Heading>
             <div>
                 <Button variant="tertiary" icon={<Email />}>Meldinger</Button>
-                <Button variant="tertiary" icon={<People />}>Min side</Button>
+                <Button variant="tertiary" 
+                    icon={<People />} 
+                    onClick={() => navigate('/')}>
+                        Min side
+                </Button>
                 <Button variant='primary' icon={ <Logout />}>Logg ut</Button>
             </div>
         </div>
