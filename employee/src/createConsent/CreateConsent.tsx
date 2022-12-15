1bf2e87e8f8b8671c9c6d1ec4b8e8f769bc6b316
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-12-15 09:18:53 +0100

    Add form validation and code generating

--- employee/src/createConsent/CreateConsent.tsx
+++ employee/src/createConsent/CreateConsent.tsx
@@ -6,7 +6,7 @@ import ConsentPreview from './components/ConsentPreview'
 import { useNavigate } from 'react-router-dom'
 import { IConsent } from '../types'
 import { getYesterdayDate, getExpirationLimitDate } from './utils/date'
-import { SubmitHandler, useForm } from 'react-hook-form'
+import { v4 as uuidv4 } from 'uuid'
 
 interface IConsentInputs {
     title: string
@@ -24,9 +24,9 @@ export default function CreateConsent(): ReactElement {
         expiration: undefined,
         candidates: []
     })
-
-    const { register, formState: { errors }, handleSubmit } = useForm<IConsentInputs>()
     
+    const [titleErrorMessage, setTitleErrorMessage] = useState<string>('')
+    const [descriptionErrorMessage, setDescriptionErrorMessage] = useState<string>('')
     const [expirationErrorMessage, setExpiraitonErrorMessage] = useState<string>('')
 
     const { datepickerProps, inputProps, selectedDay } = UNSAFE_useDatepicker({
@@ -40,8 +40,56 @@ export default function CreateConsent(): ReactElement {
         })
     }
 
-    const onCreateConsent: SubmitHandler<IConsentInputs> = (data) => {
+    const onCreateConsent = () => {
+        let isError = false
+
         // TODO: Set consent code
+        if (selectedDay) {
+            setConsent({
+                ...consent,
+                expiration: selectedDay
+            })
+            setExpiraitonErrorMessage('')
+        } else {
+            setExpiraitonErrorMessage('Du må sette en utløpsdato')
+            isError = true
+        } 
+        
+        if (consent.title.length === 0) {
+            setTitleErrorMessage('Du må sette en tittel')
+            isError = true
+        } else if (consent.title.length < 5) {
+            setTitleErrorMessage('Tittelen må være lengre enn 5 bokstaver')
+            isError = true
+        } else if (consent.title.length > 30) {
+            setTitleErrorMessage('Tittelen må være under 30 bokstaver')
+            isError = true
+        } else {
+            setTitleErrorMessage('')
+        }
+
+        if (consent.description.length === 0) {
+            setDescriptionErrorMessage('Du må sette et formål')
+            isError = true
+        } else if (consent.description.length < 30) {
+            setDescriptionErrorMessage('Formålet må være lengre en 30 bokstaver')
+            isError = true
+        } else if (consent.description.length > 300) {
+            setDescriptionErrorMessage('Formålet må være under 300 bokstaver')
+            isError = true
+        } else {
+            setDescriptionErrorMessage('')
+        }
+
+        if (!isError) {
+            let codeString = uuidv4().substring(0, 6) as string
+            codeString = `${codeString.substring(0, 3)}-${codeString.substring(3)}`.toUpperCase()
+
+            setConsent({
+                ...consent,
+                code: codeString
+            })
+        }
     }
 
 
@@ -52,51 +100,38 @@ export default function CreateConsent(): ReactElement {
                     title="Nytt samtykke"
                     icon={<Edit className='align-middle text-[2rem] absolute -top-[1rem]'/>}
                 />
-                <form className='space-y-4' onSubmit={handleSubmit(onCreateConsent)}>
-                    <div className='mt-8 space-y-4 bg-white p-4'>
-                        <TextField
-                            {...register('title', { 
-                                minLength: { 
-                                    value: 5, message: 'Tittelen må være lengre enn 5 bokstaver'
-                                },
-                                maxLength: {
-                                    value: 30, message: 'Tittelen må være under 30 bokstaver'
-                                }})}
-                            label="Tittel"
-                            error={errors?.title?.message}
+                <Panel className='mt-8 space-y-4'>
+                    <TextField 
+                        label="Tittel"
+                        name='title'
+                        value={consent.title || ''}
+                        onChange={handleConsentChange}   
+                        error={titleErrorMessage}                  
+                    />
+                    <Textarea 
+                        label="Formålet med samtykket"
+                        name='description'
+                        value={consent.description || ''}
+                        onChange={handleConsentChange}
+                        error={descriptionErrorMessage}
+                    />
+                    <UNSAFE_DatePicker {...datepickerProps} 
+                        disabled={[
+                            { from: new Date('Jan 1 1964'), to: getYesterdayDate() },
+                            { from: getExpirationLimitDate(), to: new Date('Jan 1 2088')}
+                        ]}
+                    >
+                        <UNSAFE_DatePicker.Input 
+                            {...inputProps} 
+                            label="Utløpsdato"
+                            error={expirationErrorMessage}
                         />
-                        <Textarea
-                            {...register('description', {
-                                minLength: {
-                                    value: 20,
-                                    message: 'Formålet må være lengre enn 20 bokstaver'
-                                },
-                                maxLength: {
-                                    value: 300,
-                                    message: 'Formålet må være under 300 bokstaver'
-                                }
-                            })}
-                            label="Formålet med samtykket"
-                            error={errors?.description?.message}
-                        />
-                        <UNSAFE_DatePicker {...datepickerProps} 
-                            disabled={[
-                                { from: new Date('Jan 1 1964'), to: getYesterdayDate() },
-                                { from: getExpirationLimitDate(), to: new Date('Jan 1 2088')}
-                            ]}
-                        >
-                            <UNSAFE_DatePicker.Input 
-                                {...inputProps} 
-                                label="Utløpsdato"
-                                error={expirationErrorMessage}
-                            />
-                        </UNSAFE_DatePicker>
-                    </div>
-                    <div className='flex justify-between mt-4 px-2'>
-                        <Button variant='secondary' onClick={() => navigate('/')}>Avbryt</Button>
-                        <Button type="submit">Opprett samtykkeskjema</Button>
-                    </div>
-                </form>
+                    </UNSAFE_DatePicker>
+                </Panel>
+                <div className='flex justify-between mt-4 px-2'>
+                    <Button variant='secondary' onClick={() => navigate('/')}>Avbryt</Button>
+                    <Button onClick={onCreateConsent}>Opprett samtykkeskjema</Button>
+                </div>
             </div>
             <div className='w-1/2'>
                 <ConsentPreview consent={consent}/>
