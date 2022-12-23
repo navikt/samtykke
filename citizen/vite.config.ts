9d8d88cb38e7f1f7a1b10c6e74713ebd2181925d
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-12-23 09:33:31 +0100

    Init citizen project

--- citizen/vite.config.ts
+++ citizen/vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+    plugins: [react()],
+    base: '/innbygger/',
+    server: {
+        proxy: {
+            '/innbygger/api': {
+                target: 'http://localhost:8080',
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
 })
