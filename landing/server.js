d35cbbbe006556113e01c00d30db8334fa9945b6
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2022-12-23 11:09:49 +0100

    Init landing project

---
+++ landing/server.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import path from 'path'
+import {fileURLToPath} from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const app = express()
+
+const buildPath = path.join(path.resolve(__dirname, './dist'))
+
+app.use(express.static(buildPath, { index: false }))
+
+app.get(`/isAlive|/isReady`, (req, res) => {
+    res.send('OK')
+})
+
+app.use(/^(?!.*\/(internal|static)\/).*$/, (req, res) => res.sendFile(`/index.html`))
+
+app.listen(3000, () => { console.log('Listening on port 3000')})
