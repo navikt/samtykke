755abe2bed1d25339f1fd8d33590514e99238e03
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-01-02 15:01:47 +0100

    Add base for citizen

---
+++ citizen/src/common/Header.tsx
@@ -0,0 +1,17 @@
+import React, { ReactElement } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Button, Heading } from '@navikt/ds-react'
+import { Findout, Logout } from '@navikt/ds-icons'
+
+export default function Header(): ReactElement {
+
+    return (
+        <div className='w-full m-0 flex flex-row justify-between bg-[#fefefe] p-12'>
+            <Heading size='xlarge'>Samtykke</Heading>
+            <div>
+                <Button variant='tertiary' icon={<Findout />}>Se aktive samtykker</Button>
+                <Button icon={ <Logout />}>Logg ut</Button>
+            </div>
+        </div>
+    )
+}
