fb766a0325e064b57f67728280cfc24f078f0fd8
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-01-03 14:05:27 +0100

    Add error handling to citizen submit consent code

--- citizen/src/landing/Landing.tsx
+++ citizen/src/landing/Landing.tsx
@@ -1,9 +1,35 @@
 import { FillForms } from '@navikt/ds-icons'
 import { Button, TextField } from '@navikt/ds-react'
-import React, { ReactElement } from 'react'
+import axios, { AxiosError } from 'axios'
+import React, { ReactElement, useState } from 'react'
 import PageHeader from '../common/PageHeader'
+import { IConsent } from '../types'
 
 export default function Landing(): ReactElement {
+
+    const [code, setCode] = useState<string>('')
+
+    const [codeErrorMessage, setCodeErrorMessage] = useState<string>('')
+
+    const onLoadConsent = async () => {
+        const codeRegex = /^[a-zA-Z0-9]{3}-[a-zA-Z0-9]{3}$/
+
+        if (!codeRegex.test(code)) {
+            setCodeErrorMessage('Samtykke kode er på feil format')
+        } else {
+            try {
+                const { status }: { status: number} = await axios.get(`/citizen/api/consent/${code}`)
+                if (status === 200) return // TODO: replace with navigate to consent
+            } catch (error) {
+                if (error instanceof AxiosError) {
+                    if (error.response?.status === 404) setCodeErrorMessage(`Fant ikke samtykke med kode: ${code}`)
+                    else setCodeErrorMessage('Noe gikk galt i hentingen av samtykke...')
+                } 
+            }
+        }
+
+    }
+
     return (
         <div className='mx-32 my-12'>
             <PageHeader 
@@ -15,9 +41,12 @@ export default function Landing(): ReactElement {
                     <TextField 
                         label='Samtykke-kode'
                         description='Skriv inn koden for det spesifike samtykket'
+                        value={code || ''}
+                        onChange={e => setCode(e.target.value as string)}
+                        error={codeErrorMessage}
                     />
                     <div className='flex justify-end w-full mt-2 pr-2'>
-                        <Button>OK</Button>
+                        <Button onClick={onLoadConsent}>OK</Button>
                     </div>
                 </div>
             </div>
---
+++ citizen/src/types.ts
@@ -0,0 +1,41 @@
+export interface IEmployee {
+    firstname: string
+    lastname: string
+    email: string
+    consents: IConsent[]
+    messages: IMessage[]
+}
+
+export interface IConsent {
+    title: string
+    responsibleGroup: string
+    purpose: string
+    totalInvolved: number
+    expiration: Date | undefined
+    code: string
+    candidates: ICandidate[]
+}
+
+export enum EnumCandidateStatus {
+    Accepted = 'SAMTYKKE_GITT',
+    Withdrawn = 'SAMTYKKE_TRUKKET',
+}
+
+export interface ICandidate {
+    id: string
+    name: string
+    email: string
+    status: EnumCandidateStatus
+    consented: Date | undefined
+    audioRecording: boolean
+    storeInfo: boolean
+}
+
+export interface IMessage {
+    id: number
+    timestamp: Date
+    title: string
+    description: string
+    read: boolean
+    ref?: string
+}
