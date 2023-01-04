1a96b4eb0ab7c6913775b3dedcd865d6caf7d596
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-01-04 12:25:22 +0100

    Add visuals for give and active consent

--- citizen/src/activeConsent/ActiveConsent.tsx
+++ citizen/src/activeConsent/ActiveConsent.tsx
@@ -1,7 +1,14 @@
+import { FileContent } from '@navikt/ds-icons'
 import React, { ReactElement } from 'react'
+import PageHeader from '../common/PageHeader'
 
 export default function ActiveConsent(): ReactElement {
     return (
-        <div>Hei jeg eksisterer og du har allerede gitt samtykket til meg</div>
+        <div className='mx-32 my-12'>
+            <PageHeader 
+                title="Se samtykke"
+                icon={<FileContent className='align-middle text-[2rem] absolute -top-[1rem]'/>}
+            />
+        </div>
     )
 }
--- citizen/src/giveConsent/GiveConsent.tsx
+++ citizen/src/giveConsent/GiveConsent.tsx
@@ -1,7 +1,14 @@
+import { FillForms } from '@navikt/ds-icons'
 import React, { ReactElement } from 'react'
+import PageHeader from '../common/PageHeader'
 
 export default function GiveConsent(): ReactElement {
     return (
-        <div>Hei du har IKKE gitt samtykke til meg</div>
+        <div className='mx-32 my-12'>
+            <PageHeader 
+                title="Gi samtykke"
+                icon={<FillForms className='align-middle text-[2rem] absolute -top-[1rem]'/>}
+            />
+        </div>
     )
 }
