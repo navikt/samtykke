8c97f438a18affeb998a152faa872db65f7a4235
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-01-04 15:33:19 +0100

    Add all visual for giving consent

--- citizen/src/giveConsent/GiveConsent.tsx
+++ citizen/src/giveConsent/GiveConsent.tsx
@@ -1,4 +1,5 @@
 import { FillForms } from '@navikt/ds-icons'
+import { Checkbox, CheckboxGroup, ConfirmationPanel, Heading, Panel, TextField } from '@navikt/ds-react'
 import React, { ReactElement } from 'react'
 import PageHeader from '../common/PageHeader'
 import ConsentSkeleton from '../consent/ConsentSkeleton'
@@ -13,6 +14,24 @@ export default function GiveConsent({ consent }: { consent: IConsent}): ReactEle
             />
             <div className='mt-8'>
                 <ConsentSkeleton consent={consent}/>
+                <Panel className='space-y-4'>
+                    <CheckboxGroup
+                        legend='Du takker ja til'
+                        description='Kryss av boksene du føler deg komfertabel med'
+                    >
+                        <Checkbox>Ja, dere kan ta lydopptak</Checkbox>
+                        <Checkbox>
+                        Ja, dere kan beholde kontakteinformasjonen min i intill 6 måneder
+                        i tilfelle det er behov for en oppfølgingssamtale
+                        </Checkbox>
+                    </CheckboxGroup>
+                    <TextField label='Ditt navn' className='w-1/2'/>
+                    <TextField label='Din e-post' className='w-1/2'/>
+                    <Heading size='small'>Samtykke</Heading>
+                    <ConfirmationPanel label='Ja, jeg samtykker'>
+                        {`Jeg ønsker å delta i: ${consent.title}, og har lest og forstått samtykke`}
+                    </ConfirmationPanel>
+                </Panel>
             </div>
         </div>
     )
