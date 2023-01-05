4653aa168c73f551312ddb15a21b551bbdd4b9b0
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-01-05 12:40:16 +0100

    Push to get help

--- citizen/src/giveConsent/GiveConsent.tsx
+++ citizen/src/giveConsent/GiveConsent.tsx
@@ -1,11 +1,53 @@
 import { FillForms } from '@navikt/ds-icons'
-import { Checkbox, CheckboxGroup, ConfirmationPanel, Heading, Panel, TextField } from '@navikt/ds-react'
-import React, { ReactElement } from 'react'
+import { Button, Checkbox, CheckboxGroup, ConfirmationPanel, Heading, Panel, TextField } from '@navikt/ds-react'
+import { useNavigate } from 'react-router-dom'
+import React, { ChangeEvent, ReactElement, useState } from 'react'
 import PageHeader from '../common/PageHeader'
 import ConsentSkeleton from '../consent/ConsentSkeleton'
-import { IConsent } from '../types'
+import { EnumCandidateStatus, ICandidate, IConsent } from '../types'
 
 export default function GiveConsent({ consent }: { consent: IConsent}): ReactElement {
+    
+    const navigate = useNavigate()
+    
+    const [candidate, setCandidate] = useState<ICandidate>({
+        id: '',
+        name: '',
+        email: '',
+        status: EnumCandidateStatus.Withdrawn,
+        consented: undefined,
+        audioRecording: false,
+        storeInfo: false
+    })
+
+    const handleConsentCheckboxChange = (values: string[]) => {
+        if (values.includes('audioRecording')) {
+            console.log(values)
+            setCandidate({...candidate, audioRecording: true})
+            console.log('got here')
+        } else {
+            setCandidate({...candidate, audioRecording: false})
+            console.log('should definetly not get here')
+        }
+
+        if (values.includes('storeInfo')) {
+            setCandidate({...candidate, storeInfo: true})
+        } else {
+            setCandidate({...candidate, storeInfo: false})
+        }
+    }
+
+    const handleConsentTextFieldsChange = (event: ChangeEvent<HTMLInputElement>) => {
+        setCandidate({
+            ...candidate,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const onGiveConsent = () => {
+        console.log(candidate.audioRecording)
+    }
+
     return (
         <div className='mx-32 my-12'>
             <PageHeader 
@@ -18,20 +60,39 @@ export default function GiveConsent({ consent }: { consent: IConsent}): ReactEle
                     <CheckboxGroup
                         legend='Du takker ja til'
                         description='Kryss av boksene du føler deg komfertabel med'
+                        onChange={handleConsentCheckboxChange}
                     >
-                        <Checkbox>Ja, dere kan ta lydopptak</Checkbox>
-                        <Checkbox>
+                        <Checkbox value='audioRecording'>
+                            Ja, dere kan ta lydopptak
+                        </Checkbox>
+                        <Checkbox value='storeInfo'>
                         Ja, dere kan beholde kontakteinformasjonen min i intill 6 måneder
                         i tilfelle det er behov for en oppfølgingssamtale
                         </Checkbox>
                     </CheckboxGroup>
-                    <TextField label='Ditt navn' className='w-1/2'/>
-                    <TextField label='Din e-post' className='w-1/2'/>
+                    <TextField 
+                        className='w-1/2'
+                        label='Ditt navn'
+                        name='name'
+                        value={candidate.name || ''}
+                        onChange={handleConsentTextFieldsChange}
+                    />
+                    <TextField 
+                        className='w-1/2'
+                        label='Din e-post'
+                        name='email'
+                        value={candidate.email || ''}
+                        onChange={handleConsentTextFieldsChange}
+                    />
                     <Heading size='small'>Samtykke</Heading>
                     <ConfirmationPanel label='Ja, jeg samtykker'>
                         {`Jeg ønsker å delta i: ${consent.title}, og har lest og forstått samtykke`}
                     </ConfirmationPanel>
                 </Panel>
+                <div className='flex justify-between my-4 px-2'>
+                    <Button variant="secondary" onClick={() => navigate('/')}>Avbryt</Button>
+                    <Button onClick={onGiveConsent}>Gi samtykke</Button>
+                </div>
             </div>
         </div>
     )
