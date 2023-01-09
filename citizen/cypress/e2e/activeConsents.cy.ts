767f4023acb78d9487ff0a97d11e1a1c1599d272
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-01-09 08:42:39 +0100

    Add tests for active consents page

---
+++ citizen/cypress/e2e/activeConsents.cy.ts
@@ -0,0 +1,29 @@
+describe('active consents behaves as expected', () => {
+    it('loads list of active consents', () => {
+        cy.visit(`${Cypress.env('HOST')}#/samtykker`)
+
+        cy.get('*[class^="navds-panel"]')
+            .eq(1)
+            .get('a')
+            .should('have.length', 2)
+
+        cy.get('a').eq(0).should('have.text', 'Brukertest av NAV.no')
+        cy.get('a').eq(1).should('have.text', 'Test av ny AAP kalkulator')
+    })
+
+    it('link to active consent routes to expected consent', () => {
+        cy.visit(`${Cypress.env('HOST')}#/samtykker`)
+
+        cy.get('a').eq(0).click()
+        cy.get('*[class^="navds-heading navds-heading--medium"]')
+            .eq(0)
+            .should('have.text', 'Samtykke for: Brukertest av NAV.no')
+
+        cy.visit(`${Cypress.env('HOST')}#/samtykker`)
+
+        cy.get('a').eq(1).click()
+        cy.get('*[class^="navds-heading navds-heading--medium"]')
+            .eq(0)
+            .should('have.text', 'Samtykke for: Test av ny AAP kalkulator')
+    })
+})
