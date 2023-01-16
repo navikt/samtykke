ff97ac625534df9b8176be5b09473cffc4cbd30a
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-01-16 09:02:52 +0100

    Fix footer for citizen

--- citizen/src/common/Footer.tsx
+++ citizen/src/common/Footer.tsx
@@ -4,7 +4,7 @@ import React, { ReactElement } from 'react'
 
 export default function Footer(): ReactElement {
     return (
-        <div className='flex flex-row justify-between m-0 p-12 bg-[#e0d8e9] absolute bottom-0 w-full'>
+        <div className='flex flex-row justify-between m-0 p-12 bg-[#e0d8e9] bottom-0 w-full'>
             <div className='flex flex-row items-center space-x-2'>
                 <Heading size="medium">Laget av #ResearchOps med kjærleik</Heading>
                 <HandsHeart className='text-[2rem]' />
