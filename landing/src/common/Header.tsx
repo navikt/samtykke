16368af42737a069b2b184e85e0eff768e604c33
Martin Berglund <martin.berglund@nav.no>
2023-01-26 14:11:28 +0100

    Update layout

--- landing/src/common/Header.tsx
+++ landing/src/common/Header.tsx
@@ -4,17 +4,18 @@ import { Button, Heading } from '@navikt/ds-react'
 import { Login } from '@navikt/ds-icons'
 
 export default function Header(): ReactElement {
-
     const navigate = useNavigate()
 
     return (
-        <div className='w-full m-0 flex flex-row justify-between bg-[#fefefe] p-12'>
+        <div className="flex flex-wrap items-center gap-4 justify-between bg-[#fefefe] p-4 lg:p-12 w-full">
             <button onClick={() => navigate('/')}>
-                <Heading size='xlarge' className='text-black'>Samtykke</Heading>
+                <Heading as="span" size="xlarge" className="text-black">
+                    Samtykke
+                </Heading>
             </button>
             <div>
-                <Button 
-                    variant='primary' 
+                <Button
+                    variant="primary"
                     icon={<Login aria-hidden />}
                     onClick={() => navigate('/innlogging')}
                 >
--- landing/src/landing/Landing.tsx
+++ landing/src/landing/Landing.tsx
@@ -1,30 +1,32 @@
 import { Right } from '@navikt/ds-icons'
-import { Button, Heading } from '@navikt/ds-react'
-import React, { ReactElement } from 'react'
+import { Ingress, Button, Heading } from '@navikt/ds-react'
+import { ReactElement } from 'react'
 import { useNavigate } from 'react-router-dom'
 import reops from '../assets/reops.png'
 
 export default function Landing(): ReactElement {
-
     const navigate = useNavigate()
 
     return (
-        <div className='bg-[#75c4c5] h-[calc(100vh-148px)]'>
-            <div className='flex flex-col items-center py-12 space-y-4'>
-                <Heading size='xlarge'>Gi ditt samtykke</Heading>
-                <div className='text-center px-64'>
-                    <Heading size="small">
-                        I samtykke-løsningen kan du gi ditt digitale samtykke til
-                        brukertester og innsiktsarbeid for forbedringen av NAVs tjenester
+        <div className="bg-[#75c4c5] px-4 py-12 flex-1">
+            <div className="max-w-screen-xl mx-auto">
+                <div className="max-w-prose mx-auto text-center grid gap-4 justify-items-center">
+                    <Heading level="1" size="xlarge">
+                        Gi ditt samtykke
                     </Heading>
+                    <Ingress className="text-center">
+                        I samtykke-løsningen kan du gi ditt digitale samtykke
+                        til brukertester og innsiktsarbeid for forbedringen av
+                        NAVs tjenester
+                    </Ingress>
+                    <Button
+                        icon={<Right aria-hidden />}
+                        iconPosition="right"
+                        onClick={() => navigate('/innlogging')}
+                    >
+                        Til samtykke
+                    </Button>
                 </div>
-                <Button 
-                    icon={<Right aria-hidden />} 
-                    iconPosition="right"
-                    onClick={() => navigate('/innlogging')}
-                >
-                    Til samtykke
-                </Button>
             </div>
             <img
                 src={reops}
