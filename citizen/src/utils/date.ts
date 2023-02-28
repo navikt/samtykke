00ca9bf2f2e401b6153a5d684af1943384da948a
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-02-28 14:18:35 +0100

    Update get iso date function

--- citizen/src/utils/date.ts
+++ citizen/src/utils/date.ts
@@ -1,5 +1,5 @@
 import { format, parseISO } from 'date-fns'
 
-export const getISODateString = (date: Date): string => {
-    return format(parseISO(date.toString()), 'dd.MM.yyyy')
+export const getISODateString = (date: Date | string): string => {
+    return format(parseISO(date instanceof Date ? date.toString() : date), 'dd.MM.yyyy')
 }
