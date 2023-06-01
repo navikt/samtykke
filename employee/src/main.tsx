3066c45f0cf7f1dd46a61292d68ae012759da32e
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-06-01 11:13:51 +0200

    Ugrade to react-router v6 and use switch to browser router

--- employee/src/main.tsx
+++ employee/src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter, Route, Routes } from 'react-router-dom'
-import Header from './common/Header'
+import { createBrowserRouter, createRoutesFromElements, HashRouter, Outlet, Route, RouterProvider, Routes } from 'react-router-dom'
 import './index.css'
 import '@navikt/ds-css'
 import Landing from './landing/Landing'
@@ -10,6 +9,7 @@ import CreateConsent from './createConsent/CreateConsent'
 import ActiveConsent from './activeConsent/ActiveConsent'
 import Messages from './messages/Messages'
 import config from './config'
+import Skeleton from './common/Skeleton'
 
 if (config.shouldMockAPI === 'ja') {
     worker.start({
@@ -19,16 +19,37 @@ if (config.shouldMockAPI === 'ja') {
     })
 }
 
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Skeleton />,
+        children: [
+            {
+                path: '/',
+                element: <Landing />
+            },
+            {
+                path: '/samtykke/ny',
+                element: <CreateConsent />
+            },
+            {
+                path: '/samtykke/:code',
+                element: <ActiveConsent />
+            },
+            {
+                path: '/meldinger',
+                element: <Messages />
+            }
+        ],
+    }
+], {
+    basename: '/ansatt/'
+})
+
+
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <HashRouter>
-            <Header />
-            <Routes>
-                <Route path="/" element={<Landing />}/>
-                <Route path="/samtykke/ny" element={<CreateConsent />} />
-                <Route path="/samtykke/:code" element={<ActiveConsent />} />
-                <Route path="/meldinger" element={<Messages />} />
-            </Routes>
-        </HashRouter>
+        <RouterProvider router={router}/>
     </React.StrictMode>
 )
--- employee/tests/activeConsent.spec.ts
+++ employee/tests/activeConsent.spec.ts
@@ -1,9 +1,26 @@
-import test from '@playwright/test'
+import test, { expect } from '@playwright/test'
 
 test('routes to correct active consent from landing', async ({ page }) => {
     await page.goto(process.env.HOST!)
 
     await page.getByText('Brukertest av NAV.no').click()
 
-    await page.getByText('Brukertest av NAV.no').locator('.navds-button').click()
+    await page.locator('.navds-accordion > .navds-accordion__item').nth(0).locator('.navds-button').click()
+
+    await expect(
+        page.locator('.navds-heading--xlarge').nth(1)
+    ).toHaveText('Brukertest av NAV.no')
+})
+
+test('loads expected active consent with correct data', async ({ page }) => {
+    await page.goto(`${process.env.HOST!}#/samtykke/X76-2B3`)
+
+    await expect(
+        page.locator('.navds-accordion > .navds-accordion__item > .navds-accordion__header')
+    ).toHaveText([
+        'Lars Pølse - A99E7BA3',
+        'Ole Bolle Brus - 081F1100',
+        'Navn trukket - 2E32FA72',
+        'Navn trukket - 5D0A3EA2'
+    ])
 })
