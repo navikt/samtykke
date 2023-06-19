7e7ef40cbb8c0f0e7adeecbc92ff22ddaf109672
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-06-19 13:14:40 +0200

    Refactor receipt tests

--- citizen/cypress/e2e/receipt.cy.ts
+++ citizen/cypress/e2e/receipt.cy.ts
@@ -2,74 +2,41 @@ describe('receipt handles as expected', () => {
     it('after giving consent, correct receipt is displayed', () => {
         cy.visit(`${Cypress.env('HOST')}samtykke/L90-12N`)
 
-        cy.get('input[name="name"]').type('Lars pølse')
-        cy.get('input[name="email"]').type('lars.pølse@gmail.com')
+        cy.findByRole('textbox', { name: 'Ditt navn' }).type('Lars pølse')
+        cy.findByRole('textbox', { name: 'Din e-post' }).type('Lars.pølse@outlook.com')
 
-        cy.get('*[class^="navds-checkbox navds-checkbox--medium"]')
-            .eq(1)
-            .click()
+        cy.findByRole('checkbox', { name: 'Ja, jeg samtykker' }).click()
 
-        cy.get(
-            '*[class^="navds-button navds-button--primary navds-button--medium"]',
-        )
-            .eq(1)
-            .click()
+        cy.findByRole('button', { name: 'Gi samtykke' }).click()
 
         cy.location().should((loc) => {
             expect(loc.href).to.equal(`${Cypress.env('HOST')}kvitering`)
         })
 
-        cy.get('p')
-            .eq(0)
-            .should(
-                'have.text',
-                'Du har gitt samtykke til: Test av ny AAP kalkulator. Vi setter pris på at du vil hjelpe med å gjøre NAV bedre.',
-            )
+        cy.findByText('Du har gitt samtykke til: Test av ny AAP kalkulator. Vi setter pris på at du vil hjelpe med å gjøre NAV bedre.')
     })
 
     it('after updating consent, correct receipt is displayed', () => {
         cy.visit(`${Cypress.env('HOST')}samtykke/X76-2B3`)
 
-        cy.get('input[name="name"]').type('foiha')
+        cy.findByRole('textbox', { name: 'Ditt navn' }).type('Jens Monsen')
+        cy.findByRole('button', { name: 'Oppdater' }).click()
 
-        cy.get(
-            '*[class^="navds-button navds-button--secondary navds-button--medium"]',
-        )
-            .eq(1)
-            .click()
-
-        cy.get('p').should(
-            'have.text',
-            'Samtykket ditt til: Brukertest av NAV.no har blitt oppdater, og ansatte tilknyttet ditt samtykke har blitt varslet og vil oppdatere ekstern data.',
-        )
+        cy.findByText('Samtykket ditt til: Brukertest av NAV.no har blitt oppdater, og ansatte tilknyttet ditt samtykke har blitt varslet og vil oppdatere ekstern data.')
     })
 
     it('after withdrawing consent, correct receipt is displayed', () => {
         cy.visit(`${Cypress.env('HOST')}samtykke/X76-2B3`)
 
-        cy.get(
-            '*[class^="navds-button navds-button--danger navds-button--medium"]',
-        ).click()
+        cy.findByRole('button', { name: 'Trekk samtykke' }).click()
+        cy.findByRole('button', { name: 'Trekk' }).click()
 
-        cy.get(
-            '*[class^="navds-button navds-button--danger navds-button--medium"]',
-        )
-            .eq(1)
-            .contains('Trekk')
-            .click()
-
-        cy.get('p').should(
-            'have.text',
-            'Samtykket ditt til: Brukertest av NAV.no har blitt trukket og all data knyttet til samtykket vil bli slettet/anonymisert. Ansatte knyttet til samtykket har blitt varslet og vil slette all ekstern data.',
-        )
+        cy.findByText('Samtykket ditt til: Brukertest av NAV.no har blitt trukket og all data knyttet til samtykket vil bli slettet/anonymisert. Ansatte knyttet til samtykket har blitt varslet og vil slette all ekstern data.')
     })
 
     it('if not routing from a consent, reciept should not be displayed', () => {
         cy.visit(`${Cypress.env('HOST')}kvitering`)
 
-        cy.get('*[class^="navds-heading navds-heading--large"]').should(
-            'have.text',
-            'Ingen kvitering å vise',
-        )
+        cy.findByText('Ingen kvitering å vise')
     })
 })
