8d5f743602d3359b08f25341c7916295a8a30bb6
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-06-22 11:35:24 +0200

    Update give consent form to use react-hook-forms

--- citizen/src/giveConsent/GiveConsent.tsx
+++ citizen/src/giveConsent/GiveConsent.tsx
@@ -9,92 +9,46 @@ import axios, { AxiosError } from 'axios'
 import { EnumConsentReceipt } from '../receipt/EnumConsentReceipt'
 import config from '../config'
 import { format } from 'date-fns'
+import { useForm } from 'react-hook-form'
+import { validEmailRegex } from '../utils/regex'
 
 export default function GiveConsent({ consent }: { consent: IConsent}): ReactElement {
     
     const navigate = useNavigate()
     
-    const [candidate, setCandidate] = useState<ICandidate>({
-        name: '',
-        email: '',
-        status: EnumCandidateStatus.Withdrawn,
-        consented: undefined,
-        audioRecording: false,
-    })
-
-    const [hasGivenConsent, setHasGivenConsent] = useState<boolean>(false)
-
-    const [nameErrorMessage, setNameErrorMessage] = useState<string>('')
-    const [emailErrorMessage, setEmailErrorMessage] = useState<string>('')
-    const [giveConsentErrorMessage, setGiveConsentErrorMessage] = useState<string>('')
-
     const [apiErrorMessage, setApiErrorMessage] = useState<string>('')
 
-    const handleConsentCheckboxChange = (values: string[]) => {
-        setCandidate(prevState => ({
-            ...prevState,
-            audioRecording: values.includes('audioRecording'),
-        }))
-    }
-
-    const handleConsentTextFieldsChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setCandidate({
-            ...candidate,
-            [event.target.name]: event.target.value
-        })
-    }
-
-    const onGiveConsent = async () => {
-        let isError = false
-
-        if (candidate.name.length === 0) {
-            setNameErrorMessage('Du må legge inn ditt navn')
-            isError = true
-        } else {
-            setNameErrorMessage('')
-        }
-
-        const validEmailRegex = /^[a-z\wæøåA-Z\wæøå0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-        if (candidate.email.length === 0) {
-            setEmailErrorMessage('Du må legge inn din e-post')
-            isError = true
-        } else if (validEmailRegex.test(candidate.email)) {
-            setEmailErrorMessage('')
-        } else {
-            setEmailErrorMessage('E-post er på ugyldig format')
-            isError = true
-        }
-
-        if (hasGivenConsent) {
-            setGiveConsentErrorMessage('')
-        } else {
-            setGiveConsentErrorMessage('For å kunne samtykke må du krysse av på at du har lest og forstått samtykke')
-            isError = true
-        }
-
-        if (!isError) {
-            try {
-                const { status } = await axios.post(`${config.apiPath}/consent/${consent.code}/canditature/`, {
-                    name: candidate.name,
-                    email: candidate.email,
-                    status: EnumCandidateStatus.Accepted,
-                    consented: format(new Date(), 'yyyy-MM-dd'),
-                    audioRecording: candidate.audioRecording,
-                })
-                if (status === 200) navigate('/kvitering', { 
-                    state: { 
-                        consent, 
-                        receiptType: EnumConsentReceipt.Accepted 
-                    } 
-                })
-            } catch (error) {
-                if (error instanceof AxiosError) {
-                    if (error.response?.status === 406) setApiErrorMessage('Noe i skjemaet er feil...')
-                    else setApiErrorMessage('Noe gikk galt i kontakten med serveren...')
-                }
+    const onGiveConsent = async (data) => {
+        try {
+            const { status } = await axios.post(`${config.apiPath}/consent/${consent.code}/canditature/`, {
+                name: data.name,
+                email: data.email,
+                status: EnumCandidateStatus.Accepted,
+                consented: format(new Date(), 'yyyy-MM-dd'),
+                audioRecording: data.audioRecording,
+            })
+            if (status === 200) navigate('/kvitering', { 
+                state: { 
+                    consent, 
+                    receiptType: EnumConsentReceipt.Accepted 
+                } 
+            })
+        } catch (error) {
+            if (error instanceof AxiosError) {
+                if (error.response?.status === 406) setApiErrorMessage('Noe i skjemaet er feil...')
+                else setApiErrorMessage('Noe gikk galt i kontakten med serveren...')
             }
         }
     }
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: {
+            name: '',
+            email: '',
+            audioRecording: false,
+            hasConsented: false
+        }
+    })
 
     return (
         <div className='flex-1 mt-10 px-4 lg:mt-10 lg:px-12'>
@@ -104,51 +58,69 @@ export default function GiveConsent({ consent }: { consent: IConsent}): ReactEle
             />
             <div className='mt-8'>
                 <ConsentSkeleton consent={consent}/>
-                <Panel className='space-y-4'>
-                    <CheckboxGroup
-                        legend='Du takker ja til'
-                        description='Kryss av boksene du føler deg komfertabel med'
-                        onChange={handleConsentCheckboxChange}
-                    >
-                        <Checkbox value='audioRecording'>
-                            Ja, dere kan ta lydopptak
-                        </Checkbox>
-                    </CheckboxGroup>
-                    <TextField 
-                        className='lg:w-1/2'
-                        label='Ditt navn'
-                        name='name'
-                        value={candidate.name || ''}
-                        onChange={handleConsentTextFieldsChange}
-                        error={nameErrorMessage}
-                    />
-                    <TextField 
-                        className='lg:w-1/2'
-                        label='Din e-post'
-                        name='email'
-                        value={candidate.email || ''}
-                        onChange={handleConsentTextFieldsChange}
-                        error={emailErrorMessage}
-                    />
-                    <Heading size='small'>Samtykke</Heading>
-                    <ConfirmationPanel 
-                        label='Ja, jeg samtykker'
-                        checked={hasGivenConsent || false}
-                        onChange={() => setHasGivenConsent((prevState) => !prevState)}
-                        error={!hasGivenConsent && giveConsentErrorMessage}
-                    >
-                        {`Jeg ønsker å delta i: ${consent.title}, og har lest og forstått samtykke`}
-                    </ConfirmationPanel>
-                </Panel>
-                <div className='flex justify-between my-4 px-2'>
-                    <Button variant="secondary" onClick={() => navigate('/')}>Avbryt</Button>
-                    <Button onClick={onGiveConsent}>Gi samtykke</Button>
-                </div>
-                {apiErrorMessage && (
-                    <Alert variant="error">
-                        {apiErrorMessage}
-                    </Alert>
-                )}
+                <form onSubmit={handleSubmit(data => onGiveConsent(data))}>
+                    <Panel className='space-y-4'>
+                        <CheckboxGroup
+                            legend='Du takker ja til'
+                            description='Kryss av boksene du føler deg komfertabel med'
+                        >
+                            <Checkbox {...register('audioRecording')}>
+                                Ja, dere kan ta lydopptak
+                            </Checkbox>
+                        </CheckboxGroup>
+                        <TextField
+                            {...register('name', {
+                                required: {
+                                    value: true,
+                                    message: 'Du må legge inn ditt navn'
+                                }
+                            })}
+                            className='lg:w-1/2'
+                            label='Ditt navn'
+                            error={errors.name?.message}
+                        />
+                        <TextField
+                            {...register('email', {
+                                required: {
+                                    value: true,
+                                    message: 'Du må legge inn din e-post'
+                                },
+                                pattern: {
+                                    value: validEmailRegex,
+                                    message: 'E-post er på ugyldig format'
+                                }
+                            })}
+                            className='lg:w-1/2'
+                            label='Din e-post'
+                            error={errors.email?.message}
+                        />
+                        <Heading size='small'>Samtykke</Heading>
+                        <ConfirmationPanel
+                            {...register('hasConsented', {
+                                required: {
+                                    value: true,
+                                    message: 'For å kunne samtykke må du krysse av på at du har lest og forstått samtykke'
+                                },
+                            })}
+                            label='Ja, jeg samtykker'
+                            error={errors.hasConsented?.message}
+                        >
+                            {`Jeg ønsker å delta i: ${consent.title}, og har lest og forstått samtykke`}
+                        </ConfirmationPanel>
+                    </Panel>
+                    {apiErrorMessage && (
+                        <Alert 
+                            className='my-2 mx-2'
+                            variant="error"
+                        >
+                            {apiErrorMessage}
+                        </Alert>
+                    )}
+                    <div className='flex justify-between my-4 px-2'>
+                        <Button variant="secondary" onClick={() => navigate('/')}>Avbryt</Button>
+                        <Button>Gi samtykke</Button>
+                    </div>
+                </form>
             </div>
         </div>
     )
