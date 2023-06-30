a702cf97f7fc53a8f31a5570ca4d2dc8bde41760
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-06-30 14:11:13 +0200

    Fix?

--- citizen/src/common/Footer.tsx
+++ citizen/src/common/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer(): ReactElement {
     return (
         <footer className='flex flex-row items-center gap-4 justify-between bg-[#e0d8e9] p-4 lg:p-12 w-full'>
             <div className='flex flex-row items-center space-x-2'>
-                <Heading size="medium">Laget av #ResearchOps med kjærleik</Heading>
+                <Heading size="medium" as="span">Laget av #ResearchOps med kjærleik</Heading>
                 <HandsHeart height={'2rem'} width={'2rem'} aria-hidden />
             </div>
             <button>
