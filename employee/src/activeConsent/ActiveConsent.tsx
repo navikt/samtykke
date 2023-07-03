7022646a841e2358f4e959f8c5bb595ecd2dd40b
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-07-03 15:17:32 +0200

    Test adding skeleton to employee active consent

--- employee/src/activeConsent/ActiveConsent.tsx
+++ employee/src/activeConsent/ActiveConsent.tsx
@@ -1,5 +1,5 @@
 import { FileContent } from '@navikt/ds-icons'
-import { Alert, Heading, Panel } from '@navikt/ds-react'
+import { Alert, Heading, Panel, Skeleton } from '@navikt/ds-react'
 import axios, { AxiosError } from 'axios'
 import React, { ReactElement, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
@@ -16,44 +16,47 @@ export default function ActiveConsent(): ReactElement {
     
     const { code } = useParams()
     
-    const { data: consent } = useSWR(`${config.apiPath}/consent/${code}`, fetcher<IConsent>)
+    const { data: consent, isLoading } = useSWR(`${config.apiPath}/consent/${code}`, fetcher<IConsent>)
     
     const [apiErrorMessage, setApiErrorMessage] = useState<string>('')
 
     return (
         <main className='flex flex-col mt-10 px-4 lg:mt-10 lg:px-12'>
-            {consent ? (
+            {isLoading ? <Skeleton variant='rectangle' width='100%' height={700} /> : 
                 <>
-                    <PageHeader 
-                        title={consent.title}
-                        icon={<FileContent />}
-                    >
-                        <div className='flex flex-col lg:flex-row place-items-center lg:justify-evenly space-y-4'>
-                            <Heading size="medium" as="span">Kode: {consent.code}</Heading>
-                            <Heading size="medium" as="span">
-                                Utløper: {getISODateString(consent.expiration)}
-                            </Heading> 
-                        </div>
-                    </PageHeader>
-                    <Panel className='mt-8'>
-                        <Heading size="large" className='p-4' level="2">Kandidater</Heading>
-                        <CandidatesList candidates={consent.candidates}/>
-                    </Panel>
-                    <div className='flex justify-end my-4 px-2 space-x-4'>
-                        {apiErrorMessage && (
-                            <Alert variant='error'>
-                                {apiErrorMessage}
-                            </Alert>
-                        )}
-                        <DownloadPdfButton 
-                            setApiErrorMessage={setApiErrorMessage}
-                            consentCode={code!}
-                            consentTitle={consent.title}
-                        />
-                    </div>
+                    {consent ? (
+                        <>
+                            <PageHeader 
+                                title={consent.title}
+                                icon={<FileContent />}
+                            >
+                                <div className='flex flex-col lg:flex-row place-items-center lg:justify-evenly space-y-4'>
+                                    <Heading size="medium" as="span">Kode: {consent.code}</Heading>
+                                    <Heading size="medium" as="span">
+                                    Utløper: {getISODateString(consent.expiration)}
+                                    </Heading> 
+                                </div>
+                            </PageHeader>
+                            <Panel className='mt-8'>
+                                <Heading size="large" className='p-4' level="2">Kandidater</Heading>
+                                <CandidatesList candidates={consent.candidates}/>
+                            </Panel>
+                            <div className='flex justify-end my-4 px-2 space-x-4'>
+                                {apiErrorMessage && (
+                                    <Alert variant='error'>
+                                        {apiErrorMessage}
+                                    </Alert>
+                                )}
+                                <DownloadPdfButton 
+                                    setApiErrorMessage={setApiErrorMessage}
+                                    consentCode={code!}
+                                    consentTitle={consent.title}
+                                />
+                            </div>
+                        </>
+                    ) : <Heading size='medium' as="span">Fant ikke et samtykke med kode: {code}</Heading>} 
                 </>
-            ) : <Heading size='medium' as="span">Fant ikke et samtykke med kode: {code}</Heading> 
-            } 
+            }
         </main>
     )
 }
