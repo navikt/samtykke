3a11d16892be99f84aca71db3a5ccfd59a1c6f2b
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-07-04 12:18:37 +0200

    Update data fetching in employee landing

--- employee/src/landing/Landing.tsx
+++ employee/src/landing/Landing.tsx
@@ -1,7 +1,6 @@
 import { People } from '@navikt/ds-icons'
-import { Accordion, BodyShort, Button, Heading, Panel, Skeleton } from '@navikt/ds-react'
-import axios, { AxiosError } from 'axios'
-import React, { ReactElement, useEffect, useState } from 'react'
+import { BodyShort, Button, Heading, Panel, Skeleton } from '@navikt/ds-react'
+import React, { ReactElement } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { IConsent, IEmployee } from '../types'
 import ConsentPreviews from './components/ConsentPreviews'
@@ -10,9 +9,6 @@ import useSWR, { SWRResponse } from 'swr'
 import { fetcher } from '../utils/fetcher'
 
 export default function Landing(): ReactElement {
-    
-    const [activeConsents, setActiveConsent] = useState<IConsent[]>()
-    const [activeConsentsError, setActiveConsentsError] = useState<string>()
 
     const navigate = useNavigate()
 
@@ -22,21 +18,11 @@ export default function Landing(): ReactElement {
         error: employeeError
     }: SWRResponse<IEmployee, boolean, boolean> = useSWR(`${config.apiPath}/currentEmployee`, fetcher<IEmployee>)
 
-    const getActiveConsents = async () => {
-        try {
-            const { data }: { data: IConsent[] } = await axios.get(`${config.apiPath}/consent/active`)
-            setActiveConsent(data)
-        } catch (error) {
-            if (error instanceof AxiosError) {
-                if (error.response?.status === 404) setActiveConsentsError('Du har ingen aktive samtykker...')
-                else setActiveConsentsError('Noe gikk galt i hentingen av aktive samtykker')
-            } 
-        }
-    }
-
-    useEffect(() => {
-        getActiveConsents()
-    }, [])
+    const {
+        data: consents,
+        isLoading: consentsIsLoading,
+        error: consentsError
+    }: SWRResponse<Array<IConsent>, boolean, boolean> = useSWR(`${config.apiPath}/consent/active`, fetcher<Array<IConsent>>)
 
     return (
         <main className='flex-1 mt-10 px-4 lg:mt-10 lg:px-12'>
@@ -53,17 +39,19 @@ export default function Landing(): ReactElement {
                     </>
                 }
             </div>
-            <Panel className='p-2 space-y-12'>
-                <Heading level="2" size="large">Aktive samtykker</Heading>
-                {activeConsents && activeConsents!.length > 0 ? (
-                    <ConsentPreviews consents={activeConsents} />
-                ) : (
-                    <BodyShort>{activeConsentsError}</BodyShort>
-                )}
-                <div className='flex flex-row justify-end'>
-                    <Button variant='primary' onClick={() => navigate('/samtykke/ny')}>Nytt samtykke</Button>
-                </div>
-            </Panel>
+            {consentsIsLoading ? <Skeleton variant='rectangle' width='100%' height={300}/> :
+                <Panel className='p-2 space-y-12'>
+                    <Heading level="2" size="large">Aktive samtykker</Heading>
+                    {consents && consents.length > 0 && !consentsError ? (
+                        <ConsentPreviews consents={consents} />
+                    ) : (
+                        <BodyShort>Du har ingen aktive samtykker...</BodyShort>
+                    )}
+                    <div className='flex flex-row justify-end'>
+                        <Button variant='primary' onClick={() => navigate('/samtykke/ny')}>Nytt samtykke</Button>
+                    </div>
+                </Panel>
+            }
         </main>
     )
 }
