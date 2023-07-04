7eb571379d33152aa028d10b499427352155c25c
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-07-04 12:46:28 +0200

    Update data fetching in employee messages

--- employee/src/messages/Messages.tsx
+++ employee/src/messages/Messages.tsx
@@ -1,34 +1,21 @@
 import { Email } from '@navikt/ds-icons'
-import { Accordion, BodyLong, Button, Heading, Panel } from '@navikt/ds-react'
-import axios, { AxiosError } from 'axios'
-import React, { ReactElement, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { Accordion, Heading, Panel, Skeleton } from '@navikt/ds-react'
+import React, { ReactElement } from 'react'
 import PageHeader from '../common/PageHeader'
 import { IMessage } from '../types'
 import MessageContent from './components/MessageContent'
 import MessageHeader from './components/MessageHeader'
 import config from '../config'
+import useSWR, { SWRResponse } from 'swr'
+import { fetcher } from '../utils/fetcher'
 
 export default function Messages(): ReactElement {
 
-    const [messages, setMessages] = useState<IMessage[]>()
-
-    const [messagesErrorMessage, setMessagesErrorMessage] = useState<string>('')
-
-    const getMessages = async () => {
-        try {
-            const { data }: { data: IMessage[] } = await axios.get(`${config.apiPath}/messages`)
-            setMessages(data)
-        } catch (error) {
-            if (error instanceof AxiosError) {
-                setMessagesErrorMessage('Fant ingen meldinger')
-            } else setMessagesErrorMessage('Noe gikk galt i hentingen av meldingene...')
-        }
-    }
-
-    useEffect(() => {
-        getMessages()
-    }, [])
+    const {
+        data: messages,
+        isLoading: messagesIsLoading,
+        error: messagesError
+    }: SWRResponse<Array<IMessage>, boolean, boolean> = useSWR(`${config.apiPath}/messages`, fetcher)
 
     return(
         <main className='flex flex-col mt-10 px-4 lg:mt-10 lg:px-12'>
@@ -36,20 +23,24 @@ export default function Messages(): ReactElement {
                 title='Mine meldinger'
                 icon={<Email />}
             />
-            {messages ? (
-                <Panel className='mt-8'>
-                    <Accordion>
-                        {messages.map((message: IMessage, index: number) => {
-                            return (
-                                <Accordion.Item key={index}>
-                                    <MessageHeader title={message.title} read={message.read} id={message.id}/>
-                                    <MessageContent description={message.description} reference={message.ref}/>
-                                </Accordion.Item>
-                            )
-                        })}
-                    </Accordion>
-                </Panel>
-            ) : <Heading size='medium' as="span">{messagesErrorMessage}</Heading>
+            {messagesIsLoading ? <Skeleton variant='rectangle' width='100%' height={300} /> :
+                <>
+                    {messages && messages.length > 0 && !messagesError ? (
+                        <Panel className='mt-8'>
+                            <Accordion>
+                                {messages.map((message: IMessage, index: number) => {
+                                    return (
+                                        <Accordion.Item key={index}>
+                                            <MessageHeader title={message.title} read={message.read} id={message.id}/>
+                                            <MessageContent description={message.description} reference={message.ref}/>
+                                        </Accordion.Item>
+                                    )
+                                })}
+                            </Accordion>
+                        </Panel>
+                    ) : <Heading size='medium' as="span">Fant ingen meldinger</Heading>
+                    }
+                </>
             }
         </main>
     )
