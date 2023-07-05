311e40b3a0954a52cf5c25187220c9b2738d31c6
Emil Elton Nilsen <Emil.nilsen9888@gmail.com>
2023-07-05 13:34:47 +0200

    Fix employee mock data

--- employee/src/mocks/handlers.ts
+++ employee/src/mocks/handlers.ts
@@ -18,7 +18,6 @@ const employeeMock: IEmployee = {
             code: 'X76-2B3',
             expiration: new Date(),
             endResult: 'visuell mapping',
-            slackChannelId: 'dd',
             candidates: [
                 {
                     id: '21097oifdsh',
@@ -67,7 +66,6 @@ const employeeMock: IEmployee = {
             code: 'L90-12N',
             expiration: new Date(),
             endResult: 'rapport',
-            slackChannelId: 'dd',
             candidates: [],
         },
         {
@@ -79,7 +77,6 @@ const employeeMock: IEmployee = {
             code: '12J-0ZA',
             expiration: new Date(),
             endResult: 'rapport',
-            slackChannelId: 'dd',
             candidates: [],
         },
     ],
